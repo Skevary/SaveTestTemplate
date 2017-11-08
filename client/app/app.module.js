dc80c7064a3095a82997b8100c1b658daacd4d00
Skevary <skevarys@gmail.com>
2017-11-08 16:52:30 +0200

    update

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -3,7 +3,9 @@
 
     angular.module('app', [
         // Core modules
-         'app.core'
+        'app.core'
+
+        // Custom Feature modules
 
         // 3rd party feature modules
         ,'md.data.table'
