74d8d1e115c41a15a2e45bd75927e31c62df4705
Skevary <skevarys@gmail.com>
2017-11-20 16:48:09 +0200

    Fix update Header

--- client/app/layout/appbar.controller.js
+++ client/app/layout/appbar.controller.js
@@ -5,8 +5,24 @@
         .controller('AppBarCtrl', ['$scope', AppBarCtrl]);
 
     function AppBarCtrl($scope) {
+
         $scope.$watch(function () {
+
             $scope.header = $('li.active').find('span').text();
         });
+
+        $scope.apps = [
+            {text : 'APP1', icon : 'fa-facebook-official'},
+            {text : 'APP2', icon : 'fa-linkedin-square'},
+            {text : 'APP3', icon : 'fa-twitter-square'}
+        ];
+
+        $scope.activeApp = $scope.apps[0];
+
+        $scope.setApp = function setApp(app) {
+            $scope.activeApp = app;
+
+            return $scope.activeApp;
+        };
     }
 })();
